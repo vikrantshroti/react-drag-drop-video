dcf2ac0b82c14ea446bb463b64b21177b77858d6
vikrantshroti <vikrantshroti@gmail.com>
2022-04-04 00:53:26 +0530

    reverting width and height of video to 100%

--- src/App.js
+++ src/App.js
@@ -162,8 +162,10 @@ function App() {
         style={{ backgroundColor: isDragging ? "white" : "purple" }}
       >
         <video
-          width={200}
-          height={300}
+          // width={200}
+          // height={300}
+          width={"100%"}
+          height={"100%"}
           controls
           autoPlay={false}
           id={"videoId"}
